668d66bc97553f095f7b1d69057d0446bb4246ee
Upamanyu Sharma <upamanyu@mit.edu>
2020-12-28 15:39:40 -0500

    Added (probably hard to prove) impl for increment_core

--- 1_incrserver.go
+++ 1_incrserver.go
@@ -2,6 +2,8 @@ package lockservice
 
 import (
 	ffi "./grove_ffi"
+	"fmt"
+	"github.com/tchajed/marshal"
 )
 
 type IncrServer struct {
@@ -10,27 +12,84 @@ type IncrServer struct {
 	// Address of the KVServer; for now a pointer, but in real code would be IP
 	// address, etc.
 	kvserver *KVServer
+
+	// guarded by sv.mu
+	// TODO: Clerks need to be made crash safe, by ensuring that seq's don't get reused
+	kck *KVClerk
 }
 
-// crash-safely increment counter and return the new value
-func (is *IncrServer) increment_core(args RPCVals) uint64 {
-	// TODO: impl
-	ffi.Write("", nil)
+func (is *IncrServer) increment_core_unsafe(seq uint64, args RPCVals) uint64 {
+	key := args.U64_1       // A
+	var oldv uint64         // A
+	oldv = is.kck.Get(key)  // A
+
+	is.kck.Put(key, oldv+1) // B
 	return 0
 }
 
+// crash-safely increment counter and return the new value
+//
+// Idea is this:
+// In the unsafe version, we ought to have the quadruples
+//
+// { key [kv]-> a }
+//  A
+// { key [kv]-> _ \ast v = a }
+// { key [kv]-> a }
+//
+// { key [kv]-> _ \ast v = a \ast durable_oldv = a }
+//  B
+// { key [kv]-> (a + 1) }
+// { key [kv]-> _ \ast durable_oldv = a }
+//
+// By adding code between A and B that makes durable_oldv = v, we can ensure
+// that rerunning the function will result in B starting with the correct
+// durable_oldv.
+// TODO: test this
+// Probably won't try proving this version correct (first).
+//
+func (is *IncrServer) increment_core(seq uint64, args RPCVals) uint64 {
+	key := args.U64_1
+	var oldv uint64
+	var enc marshal.Enc
 
-func (is *IncrServer) Increment(req *RPCRequest, reply *RPCReply) bool {
-	f := func(args RPCVals) uint64 {
-		return is.increment_core(args)
+	filename := "incr_request_" + fmt.Sprint(seq) + "_oldv"
+	if content := ffi.Read(filename); len(content) > 0 {
+		oldv = marshal.NewDec(ffi.Read(filename)).GetInt()
+		goto about_to_put
 	}
-	fdur := func() {
-		WriteDurableIncrServer(is)
-	}
-	r := is.sv.HandleRequest(f, fdur, req, reply)
-	return r
+
+	// XXX: This would be annoying to prove correct because the kck.Get() will
+	// blindly increment
+	// Basically, if we ever crash, we need to give pack the P to our caller;
+	// that is, we need the kv ptsto prop. To get this, we would need to
+	// "downgrade" the RPCRequestInvariant of the Get() that we're trying to do
+	// to have Pre=True and Post=True.
+	oldv = is.kck.Get(key)
+
+	enc = marshal.NewEnc(8)
+	ffi.Write(filename, enc.Finish())
+
+about_to_put:
+	is.kck.Put(key, oldv+1)
+	return 0
 }
 
+func (is *IncrServer) Increment(req *RPCRequest, reply *RPCReply) bool {
+	is.sv.mu.Lock()
+
+	if CheckReplyTable(is.sv.lastSeq, is.sv.lastReply, req.CID, req.Seq, reply) {
+	} else {
+		reply.Ret = is.increment_core(req.Seq, req.Args)
+		is.sv.lastReply[req.CID] = reply.Ret
+
+		// Want this here to write to the reply table
+		WriteDurableIncrServer(is)
+	}
+
+	is.sv.mu.Unlock()
+	return false
+}
 
 // For now, there is only one kv server in the whole world
 func WriteDurableIncrServer(ks *IncrServer) {
