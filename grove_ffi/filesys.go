505b83096cfa75d88ebd0c15d4bc60d734eca8c4
Upamanyu Sharma <upamanyu@mit.edu>
2021-03-11 08:55:57 -0500

    Fix bug in AtomicAppend()

--- grove_ffi/filesys.go
+++ grove_ffi/filesys.go
@@ -40,7 +40,7 @@ func Read(filename string) []byte {
 }
 
 func AtomicAppend(filename string, data []byte) {
-	f, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0666)
+	f, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		panic(err)
 	}
